3477ae59421550417b1b02c472cc74ae3d766894
Eric Vela <eric.vela2002@gmail.com>
2023-11-13 19:32:43 -0500

    Added pre-processing methods

--- processing.py
+++ processing.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
 
 df = pd.read_csv('data/heart.csv')
 print(df.head())
@@ -16,14 +18,32 @@ print(df.dtypes)
 
 print(df.isnull().sum())
 
+print(df.describe())
+
+print(df.corr())
+
 features = df.iloc[:, :-1]
 target = df.iloc[:, -1]
+
+smote = SMOTE()
+features_balanced, target_balanced = smote.fit_resample(features, target)
 
 features_train, features_test, target_train, target_test = train_test_split(features,target, test_size=0.15)
 
 scaler = StandardScaler()
 features_train = scaler.fit_transform(features_train)
 features_test = scaler.transform(features_test)
+
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 4, 6]
+}
+
+grid_search = GridSearchCV(RandomForestClassifier(), param_grid, cv=5)
+grid_search.fit(features_train, target_train)
+
+print("Best parameters:", grid_search.best_params_)
 
 model = RandomForestClassifier(n_estimators=100)
 
